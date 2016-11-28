f45da7c282ea9de542117d0743041e116cfea387
Wesley Nuzzo <uraniumcronorum@gmail.com>
2016-11-27 19:32:00 -0500

    Added some basic source analytics

--- logic_component/kb_tests.py
+++ logic_component/kb_tests.py
@@ -4,7 +4,42 @@ from knowledge_base import *
 from numericalTT import NumericalClaim
 
 class SourceTest(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        self.db = KnowledgeBase(NumericalClaim)
+        self.sources = [Source(str(i)) for i in range(3)]
+
+    def testTruthRanking(self):
+        self.db.addKnown(self.sources[0],5)   # 0101: q
+        self.db.addKnown(self.sources[0], 13)  # 1101: p->q
+
+        self.db.addClaim(self.sources[1], 5)  # 0101: q
+        self.db.addClaim(self.sources[1], 13)  # 1101: p->q
+        self.assertEqual(self.db.truthRanking(self.sources[1]),
+                         TruthRanking(2, 0, 0))
+        self.assertEqual(self.db.reliability(self.sources[1]),
+                         1)
+
+        self.db.addClaim(self.sources[2], 5)  # 0101: q
+        self.db.addClaim(self.sources[2], 10)  # 1010: ~q
+        self.db.addClaim(self.sources[2], 11)  # 1011: q->p
+        self.db.addClaim(self.sources[2], 14)  # 1110: p->~q
+        self.assertEqual(self.db.truthRanking(self.sources[2]),
+                         TruthRanking(1, 1, 2))
+        self.assertEqual(self.db.reliability(self.sources[2]),
+                         .5)
+        
+    def testContradictions(self):
+        self.db.addClaim(self.sources[1], 5)  # 0101: q
+        self.db.addClaim(self.sources[1], 13)  # 1101: p->q
+
+        self.db.addClaim(self.sources[2], 5)  # 0101: q
+        self.db.addClaim(self.sources[2], 10)  # 1010: ~q
+        self.db.addClaim(self.sources[2], 11)  # 1011: q->p
+        self.db.addClaim(self.sources[2], 14)  # 1110: p->~q
+
+        self.assertFalse(self.db.contradictsSelf(self.sources[1]))
+        self.assertTrue(self.db.contradictsSelf(self.sources[2]))
 
 class KnowledgeTest(unittest.TestCase):
 
--- logic_component/knowledge_base.py
+++ logic_component/knowledge_base.py
@@ -5,6 +5,7 @@ from collections import namedtuple
 import numericalTT
 
 Claim = namedtuple('Claim', 'representation source')
+TruthRanking = (namedtuple('TruthRanking', 'true false undecided'))
 
 class KnownException(Exception):
     pass
@@ -43,20 +44,44 @@ class KnowledgeBase:
 
     def claimsBySource(self, source, includeKnowns=False):
         """ Return all claims made by a certain source"""
-        out = [claim for claim in self.claims
+        out = [claim.representation for claim in self.claims
                if claim.source == source]
         if includeKnowns:
-            out += [claim for claim in self.knowns
+            out += [claim.representation for claim in self.knowns
                     if claim.source == source]
         return out
 
+    # Rank sources
+    def truthRanking(self, source):
+        true, false, undecided = (0,0,0)
+        for claim in self.claimsBySource(source):
+            ruling = self.trueOrFalse(claim)
+            if ruling is True:
+                true += 1
+            elif ruling is False:
+                false += 1
+            else:
+                undecided += 1
+        return TruthRanking(true, false, undecided)
+
+    def reliability(self, source):
+        ranking = self.truthRanking(source)
+        return ranking.true/(ranking.false+ranking.true)
+
+    def contradictsSelf(self, source):
+        if self.base.internalContradictions(self.base,
+                                            self.claimsBySource(source)):
+            return True
+        else:
+            return False
+
     # Apply rules
     def evaluate(self, knowns, claim):
-        return self.base.evaluate(claim,
-                                  [known.representation for known in knowns])
+        return self.base.evaluate(claim, knowns)
 
     def trueOrFalse(self, claim):
-        return self.evaluate(self.knowns, claim)
+        return self.evaluate([k.representation for k in self.knowns],
+                              claim)
 
     # output
     def __str__(self):
